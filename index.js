b426c68c61406903ec1d4b5a3e9eacc59771dc05
Anasta2222 <125214590+Anasta2222@users.noreply.github.com>
2023-02-20 15:51:46 +0300

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = 'Ой как неожиданно и приятно💕';
+});
